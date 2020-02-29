a5ebd1e454060a473b9ebed45bd77d063d4a8435
Bretton Wade <bretton.wade@gmail.com>
2020-02-29 10:02:44 -0500

    move files instead of copy - try to get modification dates from the file info as a last resort.

--- sort-images.pl
+++ sort-images.pl
@@ -2,90 +2,13 @@
 
 use warnings FATAL => 'all';
 use File::Find;
-use File::Path;
 use File::Basename;
+use File::Copy;
+use File::Path qw(make_path);
 use Cwd;
-
-my @dateTags = (
-    "CreateDate",
-    "DateCreated",
-    "DateTimeCreated",
-    "ContentCreateDate",
-    "MediaCreateDate",
-    "TrackCreateDate",
-    "GpsDateTime",
-    "GpsDateStamp"
-);
-sub getDateForFile {
-    my ($filename) = @_;
-    my $size = -s $filename;
-    print STDERR " ($size)";
-    if ($size > 0) {
-        my $dateValues = {};
-        foreach my $dateTag (@dateTags) {
-            my $exifDate = `exiftool -$dateTag "$filename" 2> /dev/null`;
-            chop ($exifDate);
-            if (length ($exifDate) > 0) {
-                $dateValues->{$dateTag} = $exifDate;
-            }
-        }
-
-        # if we got dates...
-        if (scalar (%$dateValues) > 0) {
-            foreach my $dateTag (sort keys %$dateValues)
-            {
-                my $date = $dateValues->{$dateTag};
-                $date =~ s/^[^:]*:\s*//;
-                my @dateComponents = split (/[: ]/, $date);
-                $date = join ('-', @dateComponents);
-                print STDERR " ($dateTag -> $date)";
-            }
-        } else {
-            print STDERR " (no date)";
-        }
-    }
-}
-
-sub enumerated {
-    my $filename = $File::Find::name;
-    if (-f $filename) {
-        if ($filename =~ /(.*\/)([^\/]*)/) {
-            my $path = $1;
-            my $leaf = $2;
-            if (($leaf =~ /jpe?g$/i) || ($leaf =~ /heic$/i) || ($leaf =~ /mov$/i)) {
-                print STDERR "$filename";
-                getDateForFile ($filename);
-            } elsif ($leaf =~ /icloud$/i) {
-                # get the leaf name from the plist file
-                my $leaf = `/usr/libexec/PlistBuddy -c Print:NSURLNameKey $filename`;
-                chop ($leaf);
-
-                $filename = "$path$leaf";
-                print STDERR "$filename";
-                if (($leaf =~ /jpe?g$/i) || ($leaf =~ /heic$/i) || ($leaf =~ /mov$/i)) {
-                    print STDERR " - DOWNLOAD ";
-
-                    # download the file, and then wait for it
-                    system ("brctl download $leaf");
-                    while (! -f $filename) {
-                        sleep (1);
-                        print STDERR ".";
-                    }
-                    print STDERR " - ";
-
-                    # now process it
-                    getDateForFile ($filename);
-                } else {
-                    print STDERR " - SKIP UNSUPPORTED TYPE IN CLOUD";
-                }
-            } else {
-                print STDERR "$filename - SKIP UNSUPPORTED TYPE";
-            }
-
-            print STDERR "\n";
-        }
-    }
-}
+use Cwd 'abs_path';
+use File::stat;
+use Time::Piece;
 
 # read the arguments
 my $defaultArgKey = "inputDir";
@@ -103,18 +26,148 @@ foreach my $arg (@ARGV) {
 }
 
 # get the destination for all the files
-my $inputDir = $args->{"inputDir"};
+my $inputDir = abs_path ($args->{"inputDir"});
+#$inputDir =~ s/ /\\ /g;
 if (! exists ($args->{"outputDir"})) {
     print STDERR "USAGE: sort-images.pl --inputDir path/to/input --outputDir path/to/output\n";
     exit;
 }
-my $outputDir = $args->{"outputDir"};
+my $outputDir = abs_path ($args->{"outputDir"});
+#$outputDir =~ s/ /\\ /g;
 print STDERR "INPUT ($inputDir)\nOUTPUT ($outputDir)\n";
 
-# enumerate all the files in the current sub-tree
-$File::Find::follow=1;
-my %options = ( wanted => \&enumerated,
-    follow             => 1,
-    follow_skip        => 2);
+my @dateTags = (
+    "CreateDate",
+    "DateTimeCreated",
+    "DateTimeOriginal",
+    "DateCreated",
+    "ContentCreateDate",
+    "MediaCreateDate",
+    "TrackCreateDate",
+    "GpsDateTime",
+    "GpsDateStamp",
+    "FileModificationDateTime"
+);
 
+sub resolveFile {
+    my ($path, $leaf, $type) = @_;
+
+    # ensure the path exists
+    make_path($path);
+
+    # add a counter while the file exists
+    my $result = "$path$leaf.$type";
+    my $index = 0;
+    while (-f $result) {
+        ++$index;
+        $result = "$path$leaf-$index.$type";
+    }
+    return $result;
+}
+
+sub pathFromDate {
+    my ($date, $leaf, $type) = @_;
+    # 2020-02-26-15-32-40
+    if (length ($date) >= 10) {
+        my @dateComponents = split (/-/, $date);
+        my $subPath = join ('/', $dateComponents[0], $dateComponents[1]);
+        return resolveFile ("$outputDir/$subPath/", $date, $type);
+    } else {
+        return resolveFile ("$outputDir/$date/", $leaf, $type);
+    }
+}
+
+sub getDateForFile {
+    my ($filename, $leaf, $type) = @_;
+
+    # check if the file has data
+    my $size = -s $filename;
+    print STDERR " ($size)";
+    if ($size > 0) {
+        # look for useful dates in the exif data
+        foreach my $dateTag (@dateTags) {
+            my $date = `exiftool -$dateTag "$filename" 2> /dev/null`;
+            chop ($date);
+            if (length ($date) > 0) {
+                # we got a date of some sort... process it and move the file
+                $date =~ s/^[^:]*:\s*//;
+                $date = join ('-', split (/[: ]/, $date));
+                print STDERR " ($dateTag -> $date)";
+                my $copyToPath = pathFromDate ($date, $leaf, $type);
+                print STDERR "\n  -> (MOVE TO) $copyToPath\n";
+                move ($filename, $copyToPath);
+                #copy ($filename, $copyToPath);
+                return;
+            }
+        }
+
+        my $filetime = localtime (stat($filename)->mtime);
+        my $date = $filetime->ymd ("-") . "-" . $filetime->hms ("-");
+        print STDERR " (FileCreateDate -> $date)";
+        my $copyToPath = pathFromDate ($date, $leaf, $type);
+        print STDERR "\n  -> (MOVE TO) $copyToPath\n";
+        #move ($filename, $copyToPath);
+    } else {
+        # remove the 0-size file?
+        print STDERR " (empty file)\n";
+    }
+}
+
+my $exifTypes = {};
+foreach my $exifType ("jpg", "jpeg", "gif", "mov", "heic") {
+    $exifTypes->{$exifType} = $exifType;
+}
+
+sub enumerated {
+    my $filename = $File::Find::name;
+    if ((-f "$filename") && ($filename =~ /^(.*\/)([^\/]*)$/)) {
+        my $path = $1;
+        my $leaf = $2;
+        my $type = lc (($leaf =~ /\.([^\.]*)$/) && $1);
+
+        print STDERR "\n";
+
+        if (exists ($exifTypes->{$type})){
+            print STDERR "(FILE) $filename";
+            getDateForFile ($filename, $leaf, $type);
+        } elsif ($type eq "icloud") {
+            print STDERR "(CLOUD) $filename";
+            # get the leaf name from the plist file
+            my $leaf = `/usr/libexec/PlistBuddy -c Print:NSURLNameKey "$filename"`;
+            chop ($leaf);
+            $type = lc (($leaf =~ /\.([^\.]*)$/) && $1);
+
+            $filename = "$path$leaf";
+            print STDERR "\n  -> (FILE) $filename";
+
+            if (exists ($exifTypes->{$type})){
+                print STDERR " - DOWNLOAD ";
+
+                # download the file, and then wait for it
+                system ("brctl download \"$leaf\"");
+                while (! -f $filename) {
+                    sleep (1);
+                    print STDERR ".";
+                }
+                print STDERR " - ";
+
+                # now process it
+                getDateForFile ($filename, $leaf, $type);
+            } else {
+                print STDERR " - SKIP UNSUPPORTED TYPE IN CLOUD\n";
+            }
+        } else {
+            print STDERR "(SKIP) $filename - UNSUPPORTED TYPE\n";
+        }
+    } else {
+        print STDERR "(DIRECTORY) $filename\n";
+    }
+}
+
+# enumerate all the files in the current sub-tree
+my %options = (
+    wanted => \&enumerated,
+    follow             => 1,
+    follow_skip        => 2
+);
 find(\%options, $inputDir);
